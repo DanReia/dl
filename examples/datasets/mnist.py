600f715270d9f23f54e9eb5376039a6ed19bcf76
dan.reia@gmail.com <daniel.correia@adswerve.com>
2024-03-23 17:27:09 +0200

    if main guard fixes num_workers issue

--- examples/datasets/mnist.py
+++ examples/datasets/mnist.py
@@ -2,8 +2,9 @@ from dl.utils.datasets import MNIST
 from torchvision.transforms import ToTensor
 from torch.utils.data import DataLoader
 
-train_data = MNIST(root="./data", download=False, train=True, transform=ToTensor())
+if __name__ == "__main__":
+    train_data = MNIST(root="./data", download=False, train=True, transform=ToTensor())
 
-img, target = train_data[0]
-dataloader = DataLoader(train_data, batch_size=256)
-print(next(iter(dataloader)))
+    img, target = train_data[0]
+    dataloader = DataLoader(train_data, batch_size=256, num_workers=1)
+    print(next(iter(dataloader)))
